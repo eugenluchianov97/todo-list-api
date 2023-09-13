cb82337d88be0ccd1797934dc3fac7a05805379a
Eugen Customagic <tisagiy415@soremap.com>
2023-09-13 16:27:14 +0300

    13.09.2023 16:27

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,10 @@ use Illuminate\Support\Facades\Route;
 Route::group(['prefix' =>'auth'], function(){
      Route::post('/register',\App\Http\Controllers\Auth\RegisterController::class);
      Route::post('/login',\App\Http\Controllers\Auth\LoginController::class);
-     Route::post('/logout',\App\Http\Controllers\Auth\LogoutController::class);
+
+     Route::group(['middleware' => ['auth:sanctum']], function(){
+        Route::post('/logout',\App\Http\Controllers\Auth\LogoutController::class);
+     });
 });
 
 
